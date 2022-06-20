d5c82b474108196cbd88d6c9e808e67786204934
root <mikaelmarvin191@gmail.com>
2022-06-20 12:29:21 +0200

    service dio

--- src/program1_node.cpp
+++ src/program1_node.cpp
@@ -1,6 +1,10 @@
 #include <zavrsni/program1.h>
 #include <tesseract_monitoring/environment_monitor.h>
 #include <tesseract_rosutils/plotting.h>
+#include <kortex_driver/PlayPreComputedJointTrajectory.h>
+#include <kortex_driver/PreComputedJointTrajectory.h>
+#include <kortex_driver/PreComputedJointTrajectoryElement.h>
+
 
 using namespace zavrsni;
 using namespace tesseract_rosutils;
@@ -35,6 +39,11 @@ int main(int argc, char** argv)
   std::string urdf_xml_string, srdf_xml_string;
   nh.getParam(ROBOT_DESCRIPTION_PARAM, urdf_xml_string);
   nh.getParam(ROBOT_SEMANTIC_PARAM, srdf_xml_string);
+  // service setup
+  ros::ServiceClient client = nh.serviceClient<kortex_driver::PlayPreComputedJointTrajectory>("precomputed_traj");
+  kortex_driver::PlayPreComputedJointTrajectory srv;
+  kortex_driver::PreComputedJointTrajectory precomputed_trajectory;
+  
 
   auto env = std::make_shared<tesseract_environment::Environment>();
   auto locator = std::make_shared<tesseract_rosutils::ROSResourceLocator>();
@@ -52,12 +61,32 @@ int main(int argc, char** argv)
     plotter = std::make_shared<ROSPlotting>(env->getSceneGraph()->getRoot());
 
   Program1 example(env, plotter, ifopt, debug);
+  
   auto trajectory = example.run();
   auto joint_states = trajectory.states;
   std::cout << "trajectory size = " << joint_states.size() << "\n";
+
   for(auto joint_state : joint_states)
-  {
-    std::cout << "current time = " << joint_state.time << "\n";
-    std::cout << joint_state.position << "\n\n";
+  { 
+    
+    kortex_driver::PreComputedJointTrajectoryElement traj_element;
+    traj_element.joint_accelerations(joint_state.acceleration);
+    traj_element.joint_angles(joint_state.position);
+    traj_element.joint_speeds(joint_state.velocity);
+    traj_element.time_from_start(joint_state.time);
+    precomputed_trajectory.trajectory_elements.push_back(traj_element);
   }
+  srv.request.input = precomputed_trajectory;
+
+  if (client.call(srv))
+  {
+    
+  }
+  else
+  {
+    std::cout << "nesto nije uspjelo";
+  }
+
+
+
 }
